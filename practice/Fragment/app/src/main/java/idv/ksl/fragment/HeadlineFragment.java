aa9e972d195baaaa8a88197b844e08adbb02be7e
ksl <ksl.startup@gmail.com>
2016-09-06 17:46:16 +0800

    commit Fragment

---
+++ practice/Fragment/app/src/main/java/idv/ksl/fragment/HeadlineFragment.java
@@ -0,0 +1,17 @@
+package idv.ksl.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by godie on 9/6/16.
+ */
+public class HeadlineFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.headline_view, container, false);
+    }
+}
